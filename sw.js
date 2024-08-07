d234bcbea085cd02fcd17b905e50092cb2c75cd8
crushing1111 <909171938@qq.com>
2024-08-07 13:25:12 +0800

    Site updated: 2024-08-07 13:25:12

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1723006913578";
+const VERSION = "1723008295720";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
